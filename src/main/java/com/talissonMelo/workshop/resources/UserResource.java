7b26d2701124b145d6e942fc0969745aec232d02
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-12-16 09:12:04 -0300

    User entity and resource

---
+++ src/main/java/com/talissonMelo/workshop/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.talissonMelo.workshop.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.talissonMelo.workshop.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Bob", "bob@gmail.com", "3242-3415", "12345678");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
