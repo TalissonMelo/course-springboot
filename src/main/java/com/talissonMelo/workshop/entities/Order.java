ac0d8fcb0b9539dd98652ca31c3364e174373421
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-12-30 09:10:35 -0300

    Subtotal & Total methods

--- src/main/java/com/talissonMelo/workshop/entities/Order.java
+++ src/main/java/com/talissonMelo/workshop/entities/Order.java
@@ -36,10 +36,10 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 
@@ -88,7 +88,7 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
+
 	public Payment getPayment() {
 		return payment;
 	}
@@ -97,8 +97,17 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
-	public Set<OrderItem> getItems(){
+	public Set<OrderItem> getItems() {
 		return items;
+	}
+
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSutTotal();
+		}
+		
+		return sum;
 	}
 
 	@Override
