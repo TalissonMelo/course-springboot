b2015825cae286e4e80555372921f6a05d58aeee
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-12-30 10:54:54 -0300

    Exception handling - findById

--- src/main/java/com/talissonMelo/workshop/services/UserService.java
+++ src/main/java/com/talissonMelo/workshop/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.talissonMelo.workshop.entities.User;
 import com.talissonMelo.workshop.repositories.UserRepository;
+import com.talissonMelo.workshop.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,8 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
-
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
